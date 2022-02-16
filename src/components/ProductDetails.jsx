0286a9713b5581007142c1a427d79a8681f7fcb0
Zach Javeri <zach.javeri@gmail.com>
2022-02-16 16:48:53 -0500

    Added some fake data in order to test webpage layout #36. Updated App.css so that the productDetails page uses the same styling as main page (for the time being)

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const ProductDetails = () => {
+const fakeData = {
+    name: "A product!",
+    price: 10.00,
+    description: "A product for our website!",
+    seller: "Me!"
+}
+
+const ProductDetails = ({setProductDetails}) => {
+
+
 	return (
-		<div id='home'>
-			<h1>Product Details</h1>
+		<div id='product_details'>
+			<h1>{fakeData.name}</h1>
+            <p>Seller: {fakeData.seller}</p>
+            <p>{fakeData.description}</p>
 		</div>
 	);
 };

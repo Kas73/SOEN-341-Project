5585846f9915caa8f2823cea2266b202552e14bc
Zach Javeri <zach.javeri@gmail.com>
2022-02-17 16:00:23 -0500

    Added bootstrap css so that the site looks better again #37. Changed the default port for server, so that it defaults to 5000 instead of 3000 if the port is not loaded from .env.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 //Connect to DB
 mongoose.connect(process.env.DB, { useNewUrlParser: true })

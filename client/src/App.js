21d9af499828f0900acb734839c02b2a004e01da
Zach Javeri <zach.javeri@gmail.com>
2022-02-17 16:55:21 -0500

    Added Login and signup routes back into app.js and included missing front-end files #37. Login and Signup need to be refactored to reflect the new form of App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home"
 import AddNewProduct from './components/AddNewProduct';
 import Navbar from "./components/layout/Navbar"
+import Login from "./components/Login"
+import SignUp from './components/SignUp';
 import './App.css';
 
 const App = () => {
+	const [loginInfo, setLoginInfo] = useState({});
+	const [signupInfo, setSignUp] = useState({});
+
   return (
     <BrowserRouter>
       <div className="App">
@@ -16,6 +21,16 @@ const App = () => {
             exact
             path="/create-product"
             element={<AddNewProduct />} />
+          <Route
+						exact
+						path='/login'
+						element={<Login setLoginInfo={setLoginInfo} />}
+					/>
+          <Route
+						exact
+						path='/signup'
+						element={<SignUp setSignUp={setSignUp} />}
+					/>
         </Routes>
       </div>
     </BrowserRouter>

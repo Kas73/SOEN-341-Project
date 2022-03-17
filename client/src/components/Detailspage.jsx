6e35a89ad4ac81eebc2f8cd078e97ad94e384a41
Mitchel Scott-Bond <65721975+mitchelscottbond@users.noreply.github.com>
2022-03-16 21:30:46 -0400

    Modifying Detailspages
    Changed the Detailspage from component to const function

---
+++ client/src/components/Detailspage.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import AddNewProduct from './AddNewProduct';
+import ListProducts from './ProductList';
+
+
+const Detailspage = () => {
+    const [product, setProduct] = useState({});
+
+    return(
+
+        <div id='home'>
+			<h1>HOME</h1>
+            
+		</div>
+
+
+
+    )
+
+}
+export default Detailspage

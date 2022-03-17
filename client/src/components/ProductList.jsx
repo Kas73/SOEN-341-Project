103b9b6948eb1f3510a2dae93307a44b50247ed7
SSpaceApe <omar-tawil@outlook.com>
2022-03-16 22:15:12 -0400

    added a grid to ProductList with linked names and images; placeholder for images is product.name for now

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 
 const ListProducts = ({products}) => {
     return (
-        <ul>
+        
+        
+           
+        <div className="grid-container">
+            <main className="row">
+                
             {products && products.length > 0 ? (
                 products.map((product) => {
                     return (
-                        <li key={product._id}>
-                            {product.product_name}, ${product.product_price}
-                        </li>
+                        <div key={product._id} className ="card">
+                            <a href={`/product/${product._id}`}>
+                            <img src="" alt= {product.product_name} />
+                            </a>
+                            <div className="card-body"> 
+                            <a href={`/product/${product._id}`}>{product.product_name}</a>, ${product.product_price}</div>
+                        </div>
                     )
                 })
             ) : (
                 <li>No Products(s) available</li>
             )}
-        </ul>
+            </main>
+        </div>
+    
     )
+            
+       
+    
 }
 
 export default ListProducts;

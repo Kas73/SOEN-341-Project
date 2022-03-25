233ec6a883b2df105c955eacfd6f15e07dae134c
Zach Javeri <zach.javeri@gmail.com>
2022-03-25 19:58:59 -0400

    Cart page now displays information from the database based on what the user has in their cart #90

--- client/src/components/CartRow.jsx
+++ client/src/components/CartRow.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
-function CartRow() {
+const CartRow = ({item}) => {
+	console.log(JSON.stringify(item))
 	return (
 		<tr>
 			<td colspan='4'>
-				<table class='table mb-0'>A</table>
+				<table class='table mb-0'>{item.product_name}</table>
 			</td>
 			<td colspan='4'>
-				<table class='table mb-0'>B</table>
+				<table class='table mb-0'>{item.price}</table>
 			</td>
 			<td colspan='4'>
-				<table class='table mb-0'>C</table>
+				<table class='table mb-0'>{item.quantity}</table>
 			</td>
 			<td colspan='4' className='right-align'>
 				<button className='btn btn-dark'>+</button>{' '}

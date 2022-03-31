ef3ee9a1e76aa0ad138ffbd884e2a739537724f2
HadjerHamlat <h_hamlat@live.concordia.ca>
2022-03-30 20:09:00 -0400

    Added payments mehtod object to users database

--- models/users.model.js
+++ models/users.model.js
@@ -9,6 +9,7 @@ const UserSchema = new Schema({
 	user_name: String,
 	address: String,
 	phone_no: String,
+	payment_method: [{card_number: String, cardholder_name: String, expiration: String, billing_address: String}],
 });
 
 const User = mongoose.model('users', UserSchema);
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -32,7 +32,8 @@ router.post('/users', (req, res, next) => {
 		req.body.first_name &&
 		req.body.last_name &&
 		req.body.address &&
-		req.body.phone_no
+		req.body.phone_no &&
+		req.body.payment_method
 	) {
 		Users.create({
 			user_name: req.body.user_name,
@@ -42,6 +43,7 @@ router.post('/users', (req, res, next) => {
 			last_name: req.body.last_name,
 			address: req.body.address,
 			phone_no: req.body.phone_no,
+			payment_method: req.body.payment_method,
 		})
 			.then((data) => res.json(data))
 			.catch(next);

18febee1e2537bddb48d17e90e5ee837d9f59fcd
GBCon <brodeurgabe@gmail.com>
2022-04-05 17:04:19 -0400

    Update orders.routes.js

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const Orders = require("../models/orders.model");
 const router = express.Router();
-const Products = require("../models/orders.model");
 
 router.get('/orders', (req, res, next) => {
-	Carts.find({})
+	Orders.find({})
 		.then((data) => {
 			console.log('Got order: ' + data);
 			res.json(data);
@@ -13,7 +13,7 @@ router.get('/orders', (req, res, next) => {
 
 router.get('/orders/:username', (req, res, next) => {
 	console.log(`Getting orders for Username: ${req.params.username}`);
-	Carts.findOne({ username: req.params.username })
+	Orders.findOne({ username: req.params.username })
 		.then((data) => {
 			console.log('Got orders for ' + req.params.username + ': ' + data);
 			res.json(data);
@@ -23,7 +23,7 @@ router.get('/orders/:username', (req, res, next) => {
 
 router.post("/orders", (req, res, next) => {
     if(req.body.order_id && req.body.order && req.body.addresses && req.body.order_status && req.body.username && req.body.payment_method) {
-      Products.create({
+      Orders.create({
           order_id: req.body.order_id,
           order: req.body.order,
           addresses: req.body.addresses,
@@ -40,10 +40,19 @@ router.post("/orders", (req, res, next) => {
     }
   });
 
-        router.delete("/orders/:order_id", (req, res, next) => {
-            Products.findOneAndDelete({ order_id: req.params.order_id })
-              .then((data) => res.json(data))
-              .catch(next);
-          })
+  router.patch('/orders/:order_id', (req, res, next) => {
+    console.log(`Updating order status for ${req.params.order_id} with following data: ` + JSON.stringify(req.body.cart))
+    if (req.body.order_id && req.params.order_status) {
+      Orders.findOneAndUpdate({order_id: req.body.order_id}, {
+        order_status
+      })
+      .then((data) => res.json(data))
+      .catch(next);
+    } else {
+      res.json({
+        error: 'Name or price is empty',
+      });
+    }
+  });
           
           module.exports = router;

c29cae96ecde805a9b1dc72863f7952338fde5b0
Ankita Ingle <aningle31@gmail.com>
2022-04-06 18:08:52 -0400

    Fixed the look of the cart on the checkout page. Added the functionality for showing the total sum owed.

--- client/src/components/CheckoutListRow.jsx
+++ client/src/components/CheckoutListRow.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-const CheckoutListRow = ({name, quantity, price}) => {
-
+const CheckoutListRow = ({ name, quantity, price }) => {
 	return (
-		<li className='list-group-item d-flex justify-content-between'>
-						<span>{name}</span>
-                        <span>{quantity}</span>
-						<strong>{price}</strong>
-					</li>
+		<tr>
+			<td>{name}</td>
+			<td>{quantity}</td>
+			<td>${price}</td>
+		</tr>
 	);
-}
+};
 
 export default CheckoutListRow;
